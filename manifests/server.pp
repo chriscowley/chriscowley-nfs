b5d5fe6eba75379fad37255ceddb55208cbe7208
Chris Cowley <chris@chriscowley.me.uk>
2013-06-11 22:12:42 +0200

    First commit after refactor

---
+++ manifests/server.pp
@@ -0,0 +1,43 @@
+class nfs::server { 
+  $exports = hiera("exports")
+  define list_exports {
+    $export = $name
+    file { $export:
+      ensure => directory,
+      mode   => '0775',
+      owner  => 'root',
+#      group  => 'root'
+    }
+  }
+  list_exports { $exports:; } -> File['/etc/exports']
+  package { 'rpcbind':
+    ensure => latest,
+  }
+  package { 'nfs-utils':
+    ensure => latest,
+  }
+  file { '/etc/sysconfig/nfs':
+    ensure  => present,
+    owner   => "root",
+    group   => "root",
+    mode    => "0644",
+    source  => "puppet:///modules/nfs/sysconfig-nfs",
+    require => Package['nfs-utils']
+  }
+  
+  file { '/etc/exports':
+    content => template("nfs/exports.erb"),
+  } ~> Service['nfs']
+
+  service { "rpcbind":
+    ensure => running,
+    enable => true,
+  }
+  service { "nfs":
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/sysconfig/nfs'],
+    require   => Service['rpcbind']
+  }
+}
+ 
